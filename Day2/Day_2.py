bd5a1e823bbd527284469eca930e77ff3c0d1589
sonali-12-sb <90694186+sonali-12-sb@users.noreply.github.com>
2022-01-18 23:14:04 +0530

    Day2 Assignment Submitted

---
+++ Day2/Day_2.py
@@ -0,0 +1,23 @@
+# defining a list
+list1 = []
+list1.append(22)
+list1.append(22.0)
+list1.append('Day2')
+list1.append('Sonali_Balsure')
+list1.append([1,2,3,4])
+print(list1)
+print("Item at 2nd position is: ",list1[2])
+print("Length of List is: ",len(list1))
+# defining tuple
+tuple = ("Sonali","Balsure")
+print(tuple)
+tup = len(tuple)
+print("Length of tuple is: ",tup)
+''' defining set'''
+set = {"This","is","Set"}
+print(set)
+print("Length of set is: ",len(set))
+'''dictionary'''
+dict = {"Name":"Sonali","Surname":"Balsure"}
+print(dict)
+print("Length of Dictionary is: ",len(dict))
